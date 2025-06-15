854342ba82168035be77ea22ec9691ef3d54b19c
Max Loued Baisac <raindrops.dark@gmail.com>
2025-06-15 14:28:27 +0800

    small fixes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -28,7 +28,15 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">                            
-                            <x-cloudinary::image public-id="{{ Auth::user()->profile_photo_path }}" width="50" height="50" class="rounded-full object-cover w-10 h-10" alt="Profile Photo" />
+
+                            <x-profile-photo 
+                                :path="Auth::user()->profile_photo_path" 
+                                :alt="Auth::user()->name" 
+                                class="rounded-full object-cover w-10 h-10" 
+                                width="50" 
+                                height="50" 
+                            />
+                            
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
