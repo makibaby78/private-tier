ebb3ff0dc934a9e848d4c1cb675e9902216288b6
Max Loued Baisac <raindrops.dark@gmail.com>
2025-06-15 14:03:30 +0800

    settings clean

--- resources/views/settings/partials/update-photo-form.blade.php
+++ resources/views/settings/partials/update-photo-form.blade.php
@@ -2,13 +2,18 @@
 
 <form method="POST" action="{{ route('settings.upload-photo') }}" enctype="multipart/form-data">
     @csrf
-    <div>
-        <label for="photo">Upload Profile Photo (Cloudinary)</label>
-        <input type="file" name="photo" id="photo">
+    <div class="mb-2">
+        <input type="file" name="photo" id="photo"
+        class="w-full text-slate-500 font-medium text-sm bg-gray-100 file:cursor-pointer cursor-pointer file:border-0 file:py-2 file:px-4 file:mr-4 file:bg-gray-800 file:hover:bg-gray-700 file:text-white rounded" />
     </div>
-    <button type="submit">Upload</button>
-</form>
 
-@if (auth()->user()->profile_photo_url)
-    <x-cloudinary::image public-id="{{ $user->profile_photo_path }}" width="50" height="50" class="rounded" alt="Profile Photo" />
-@endif
+    <x-profile-photo 
+        :path="$user->profile_photo_path" 
+        :alt="$user->name" 
+        class="rounded object-cover w-32 h-32 mb-2" 
+        width="50" 
+        height="50" 
+    />
+    
+    <x-primary-button>{{ __('Save') }}</x-primary-button>
+</form>
